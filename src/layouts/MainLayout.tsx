3451524aaf32b886089e7b420703a85670f62a85
Viktor Mykhalchevskyi <mikhalchevskiu@gmail.com>
2025-02-01 22:26:35 +0200

    feat: add main layout on page

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+
+
+class MainLayout extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default MainLayout;
