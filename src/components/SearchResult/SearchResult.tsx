5d04cdc3c88ae922ce880845ab12726d9a134216
Viktor Mykhalchevskyi <mikhalchevskiu@gmail.com>
2025-02-02 21:57:01 +0200

    feat: add api error handling in spinner

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import Pagination from '../Pagination/Pagination';
+import Spinner from '../Spinner/Spinner';
 interface AstronomicalObjectParams {
   name: string;
   astronomicalObjectType: string;
@@ -9,8 +10,10 @@ interface AstronomicalObjectParams {
 }
 
 interface SearchResultProps {
-  pageSizeChange: (pageSize: string) => void,
-  pageChange: (page: number) => void,
+  pageSizeChange: (pageSize: string) => void;
+  pageChange: (page: number) => void;
+  isLoading: boolean;
+  error: string | null;
 
   data: {
     page?: {
@@ -19,44 +22,53 @@ interface SearchResultProps {
       totalPages: number;
       firstPage: boolean;
       lastPage: boolean;
-    }
+    };
     astronomicalObjects?: AstronomicalObjectParams[];
   };
 }
 
 class SearchResult extends Component<SearchResultProps> {
-  componentDidUpdate(): void {
-    console.log('getData', this.props.data);
-  }
-
   render() {
     return (
-      <table>
-        <thead>
-          <th>Title</th>
-          <th>Type</th>
-        </thead>
-        <tbody>
-          {this.props.data?.astronomicalObjects
-            ? this.props.data?.astronomicalObjects.map((item) => {
-              return (
-                <tr>
-                  <td>{item.name}</td>
-                  <td>{item.astronomicalObjectType}</td>
-                </tr>
-              );
-            })
-            : null}
-        </tbody>
+      <Spinner isLoading={this.props.isLoading} error={this.props.error}>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.data?.astronomicalObjects
+              ? this.props.data?.astronomicalObjects.map((item) => {
+                  return (
+                    <tr key={item.name}>
+                      <td>{item.name}</td>
+                      <td>{item.astronomicalObjectType}</td>
+                    </tr>
+                  );
+                })
+              : null}
+          </tbody>
+        </table>
         <div>
-          <Pagination page={this.props.data?.page} setPage={this.props.pageChange} />
+          <select
+            id="table-pagination"
+            onChange={(e) => {
+              this.props.pageSizeChange(e.target.value);
+            }}
+            name="table-pagination"
+          >
+            <option value={10}>10 на сторінці</option>
+            <option value={50}>50 на сторінці</option>
+            <option value={100}>100 на сторінці</option>
+          </select>
+          <Pagination
+            page={this.props.data?.page}
+            setPage={this.props.pageChange}
+          />
         </div>
-        <select id="table-pagination" onChange={(e) => { this.props.pageSizeChange(e.target.value) }} name='table-pagination'>
-          <option value={10}>10 на сторінці</option>
-          <option value={50}>50 на сторінці</option>
-          <option value={100}>100 на сторінці</option>
-        </select>
-      </table>
+      </Spinner>
     );
   }
 }
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,85 +1,102 @@
 import { Component } from 'react';
 import TopControls from '../components/TopControls/TopControls';
 import SearchResult from '../components/SearchResult/SearchResult';
+import ErrorBtn from '../components/ErrorBtn/ErrorBtn';
+import Spinner from '../components/Spinner/Spinner';
 
 interface InitialLayoutState {
-  searchParam: string,
-  pageSize: string,
-  page: string,
+  searchParam: string;
+  pageSize: string;
+  page: string;
   searchData: object;
+  isLoading: boolean;
+  error: string | null;
 }
 class MainLayout extends Component<{}, InitialLayoutState> {
   state = {
     searchData: {},
-    searchParam: '',
+    searchParam: localStorage.getItem('searchValue') || '',
     pageSize: '10',
     page: '0',
+    isLoading: true,
+    error: null,
   };
   baseUrl = 'https://stapi.co/api/v2/rest/astronomicalObject/';
 
   componentDidMount(): void {
     this.fetchData();
   }
-  componentDidUpdate(prevProps: {},
-    prevState: Readonly<InitialLayoutState>): void {
+  componentDidUpdate({}, prevState: Readonly<InitialLayoutState>): void {
     const { searchParam, pageSize, page } = this.state;
-    console.log("updateParams", searchParam, pageSize, page)
-    if (prevState.searchParam !== searchParam ||
+    if (
+      prevState.searchParam !== searchParam ||
       prevState.pageSize !== pageSize ||
       prevState.page !== page
     ) {
+      this.setState({ isLoading: true });
       this.fetchData();
     }
   }
 
-
-
   fetchData = () => {
-    console.log("fetch with pageSize", this.state.pageSize)
     const url = new URL(this.baseUrl + 'search');
     const bodyForm = new URLSearchParams();
-    bodyForm.append("name", this.state.searchParam);
-    console.log("params: ", this.state.searchParam, this.state.pageSize, this.state.page)
+    bodyForm.append('name', this.state.searchParam);
     url.searchParams.append('name', this.state.searchParam.toString());
     url.searchParams.append('pageNumber', this.state.page.toString());
     url.searchParams.append('pageSize', this.state.pageSize.toString());
     fetch(url.toString(), {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: bodyForm.toString()
+      body: bodyForm.toString(),
     })
       .then((responce) => responce.json())
-      .then((data) => this.setState({ 
-        searchData: data,       
-        page: Math.min(Number(this.state.page), data.page.totalPages-1).toString() 
-      }));
-    console.log(this.state.searchData);
+      .then((data) => {
+        setTimeout(() => {
+          this.setState({
+            searchData: data,
+            page: Math.min(
+              Number(this.state.page),
+              data.page.totalPages - 1
+            ).toString(),
+            isLoading: false,
+          });
+        }, 1000);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        this.setState({ isLoading: false, error: error.message });
+      });
   };
 
   handlePageSizeChange = (pageSize: string) => {
     this.setState({
       pageSize: pageSize,
     });
-
-  }
+  };
   handlePageChange = (page: number) => {
     this.setState({
       page: page.toString(),
     });
-
-  }
+  };
   handleSeachParam = (seachValue: string) => {
-    this.setState({ searchParam: seachValue });
-
-  }
+    this.setState({ searchParam: seachValue.trim() });
+  };
 
   render() {
     return (
       <div>
         <TopControls onSearch={this.handleSeachParam} />
-        <SearchResult data={this.state.searchData} pageSizeChange={this.handlePageSizeChange} pageChange={this.handlePageChange} />
+        <SearchResult
+          data={this.state.searchData}
+          pageSizeChange={this.handlePageSizeChange}
+          pageChange={this.handlePageChange}
+          isLoading={this.state.isLoading}
+          error={this.state.error}
+        />
+        <ErrorBtn />
       </div>
     );
   }
