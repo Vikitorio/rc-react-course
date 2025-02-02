3053f37c5760912d6a69fc7dfa45d78f9174605f
Viktor Mykhalchevskyi <mikhalchevskiu@gmail.com>
2025-02-02 22:16:52 +0200

    feat: add basic search styles

--- src/components/TopControls/TopControls.tsx
+++ src/components/TopControls/TopControls.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-
+import style from './style.module.scss';
 interface TopControlsProps {
   onSearch: (seachValue: string) => void;
 }
@@ -18,15 +18,16 @@ class TopControls extends Component<TopControlsProps, TopControlsState> {
   };
   render() {
     return (
-      <div>
+      <div className={style['search']}>
         <input
+        className={style['search__input']}
           type="search"
           placeholder="Enter value"
           value={this.state.searchValue}
           onChange={(e) => this.setState({ searchValue: e.target.value })}
           id="site-search"
         />
-        <button onClick={this.startSearch}>Search</button>
+        <button onClick={this.startSearch} className={style['search__button']}>Search</button>
       </div>
     );
   }
