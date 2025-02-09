108d189774ddea3631f0b2662e5665f0bdc45258
Viktor Mykhalchevskyi <mikhalchevskiu@gmail.com>
2025-02-09 10:46:06 +0200

    refactor: change pagination to functional component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import style from './style.module.scss';
 interface PaginationProps {
   page?: {
@@ -13,11 +12,11 @@ interface PaginationProps {
   setPageSize: (pageSize: string) => void;
 }
 
-class Pagination extends Component<PaginationProps> {
-  getPagesArray = () => {
+const Pagination: React.FC<PaginationProps> = (props: PaginationProps) => {
+  const getPagesArray = () => {
     const pageArray: number[] = [];
-    const currentPage = this.props.page?.pageNumber || 0;
-    const totalPages = this.props.page?.totalPages || 0;
+    const currentPage = props.page?.pageNumber || 0;
+    const totalPages = props.page?.totalPages || 0;
     const pagesBack = currentPage - 3;
     let pagesUpfront = currentPage + 2;
     if (pagesBack < 0) {
@@ -33,40 +32,38 @@ class Pagination extends Component<PaginationProps> {
     return pageArray;
   };
 
-  render() {
-    return (
-      <div className={style['pagination']}>
-        <ul className={style['pagination__list']}>
-          {this.getPagesArray().map((value) => (
-            <li
-              key={value}
-              onClick={() => this.props.setPage((value - 1).toString())}
-              className={`${style['pagination__item']} ${
-                this.props.page?.pageNumber === value - 1
-                  ? style['pagination__item--active']
-                  : ''
-              }`}
-            >
-              {value}
-            </li>
-          ))}
-        </ul>
-        <select
-          id="table-pagination"
-          value={this.props.initialPageSize}
-          onChange={(e) => {
-            this.props.setPageSize(e.target.value);
-          }}
-          name="table-pagination"
-          className={style['pagination__select']}
-        >
-          <option value={10}>10</option>
-          <option value={50}>50</option>
-          <option value={100}>100</option>
-        </select>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={style['pagination']}>
+      <ul className={style['pagination__list']}>
+        {getPagesArray().map((value) => (
+          <li
+            key={value}
+            onClick={() => props.setPage((value - 1).toString())}
+            className={`${style['pagination__item']} ${
+              props.page?.pageNumber === value - 1
+                ? style['pagination__item--active']
+                : ''
+            }`}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+      <select
+        id="table-pagination"
+        value={props.initialPageSize}
+        onChange={(e) => {
+          props.setPageSize(e.target.value);
+        }}
+        name="table-pagination"
+        className={style['pagination__select']}
+      >
+        <option value={10}>10</option>
+        <option value={50}>50</option>
+        <option value={100}>100</option>
+      </select>
+    </div>
+  );
+};
 
 export default Pagination;
