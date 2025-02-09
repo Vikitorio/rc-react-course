ab7943f4942c8040cbeb2106fa0968d809f1e6f9
Viktor Mykhalchevskyi <mikhalchevskiu@gmail.com>
2025-02-09 10:31:59 +0200

    refactor: switch btn to funtional component

--- src/components/ErrorBtn/ErrorBtn.tsx
+++ src/components/ErrorBtn/ErrorBtn.tsx
@@ -1,20 +1,15 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import Button from '../Button/Button';
-class ErrorBtn extends Component {
-  state = {
-    throwError: false,
-  };
+const ErrorBtn: React.FC = () => {
+  const [isError, setIsError] = useState(false);
 
-  handleClick = () => {
-    this.setState({ throwError: true });
+  const handleClick = () => {
+    setIsError(true);
   };
-
-  render() {
-    if (this.state.throwError) {
-      throw new Error('Error happens!!!');
-    }
-    return <Button onClick={this.handleClick}>Throw Error</Button>;
+  if (isError) {
+    throw new Error('Error happens!!!');
   }
-}
+  return <>{!isError && <Button onClick={handleClick}>Throw Error</Button>}</>;
+};
 
 export default ErrorBtn;
