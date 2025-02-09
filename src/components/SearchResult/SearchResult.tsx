fe10fac6e0c8a38f88ebbc6186911aa7b5a3f43d
Viktor Mykhalchevskyi <mikhalchevskiu@gmail.com>
2025-02-09 11:21:48 +0200

    refactor: rework result table to functional component

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import Pagination from '../Pagination/Pagination';
 import Spinner from '../Spinner/Spinner';
 import style from './style.module.scss';
@@ -29,50 +28,47 @@ interface SearchResultProps {
   };
 }
 
-class SearchResult extends Component<SearchResultProps> {
-  render() {
-    return (
-      <div className={style['search-result']}>
-        <Spinner isLoading={this.props.isLoading} error={this.props.error}>
-          <table className={style['search-result__table']}>
-            <thead className={style['search-result__header']}>
-              <tr>
-                <th>Title</th>
-                <th>Type</th>
-              </tr>
-            </thead>
-            <tbody className={style['search-result__body']}>
-              {this.props.data?.astronomicalObjects
-                ? this.props.data?.astronomicalObjects.map((item) => {
-                    return (
-                      <tr
-                        key={item.name}
-                        className={style['search-result__row']}
-                      >
-                        <td className={style['search-result__cell']}>
-                          {item.name}
-                        </td>
-                        <td className={style['search-result__cell']}>
-                          {item.astronomicalObjectType}
-                        </td>
-                      </tr>
-                    );
-                  })
-                : null}
-            </tbody>
-          </table>
-          {this.props.data?.page?.totalElements && (
-            <Pagination
-              initialPageSize={Number(this.props.pageSize)}
-              page={this.props.data?.page}
-              setPage={this.props.pageChange}
-              setPageSize={this.props.pageSizeChange}
-            />
-          )}
-        </Spinner>
-      </div>
-    );
-  }
-}
+const SearchResult: React.FC<SearchResultProps> = (
+  props: SearchResultProps
+) => {
+  return (
+    <div className={style['search-result']}>
+      <Spinner isLoading={props.isLoading} error={props.error}>
+        <table className={style['search-result__table']}>
+          <thead className={style['search-result__header']}>
+            <tr>
+              <th>Title</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody className={style['search-result__body']}>
+            {props.data?.astronomicalObjects
+              ? props.data?.astronomicalObjects.map((item) => {
+                  return (
+                    <tr key={item.name} className={style['search-result__row']}>
+                      <td className={style['search-result__cell']}>
+                        {item.name}
+                      </td>
+                      <td className={style['search-result__cell']}>
+                        {item.astronomicalObjectType}
+                      </td>
+                    </tr>
+                  );
+                })
+              : null}
+          </tbody>
+        </table>
+        {props.data?.page?.totalElements && (
+          <Pagination
+            initialPageSize={Number(props.pageSize)}
+            page={props.data?.page}
+            setPage={props.pageChange}
+            setPageSize={props.pageSizeChange}
+          />
+        )}
+      </Spinner>
+    </div>
+  );
+};
 
 export default SearchResult;
